1e868c63c74a5a4ab6c9c2cc13724c217795b8b6
Sahar Atat <saharatat325@gmail.com>
2026-05-14 11:32:20 +0300

    hover overlay on image

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,14 @@
 import Banner from "@/Components/Banner";
 import GridHomePage from "@/Components/GridHomePage";
 import ContactUsHero from "@/Components/ContactUsHero";
-
+import TextOverlayImages from "@/Components/TextOverlayImages";
 
 export default function HomePage() {
   return (
     <main className="min-h-screen bg-black text-ink">
       
       <Banner />
+      <TextOverlayImages />
       <GridHomePage />
       <ContactUsHero />
       
