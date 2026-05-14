89a7aa0e2eab69918209e2e0075229d5d755f8f7
Sahar Atat <saharatat325@gmail.com>
2026-05-14 11:50:02 +0300

    responsive+adding overlay on the 2 row image

--- Components/TextOverlayImages.js
+++ Components/TextOverlayImages.js
@@ -5,7 +5,7 @@ export default function TextOverlayImages() {
         <h2 className="mb-6 text-3xl text-white">
           Explore the Universe of Movies
         </h2>
-        <div className="grid grid-cols-3 grid-rows-2 gap-4 h-[500px]">  
+        <div className="grid grid-cols-1 md:grid-cols-3 md:grid-rows-2 gap-4 md:h-[500px]">  
                 <div className="group relative overflow-hidden ">
                 <img className= "h-full w-full object-cover cursor-pointer rounded-lg overflow-clip" src="https://img.magnific.com/free-photo/ultra-detailed-nebula-abstract-wallpaper-4_1562-749.jpg?semt=ais_hybrid&w=740&q=80" alt="space" />
                 <div className="absolute bottom-0 w-full cursor-pointer bg-gradient-to-t from-black via-gray to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex flex-col justify-end p-4">
@@ -20,7 +20,13 @@ export default function TextOverlayImages() {
                 <p className="text-gray-200 text-sm">Deep space exploration</p>
                 </div>
                 </div>
-                <img src="https://png.pngtree.com/background/20250315/original/pngtree-simple-space-view-mobile-wallpaper-picture-image_15525419.jpg" alt="Description" className="row-span-2 h-full w-full object-cover rounded-lg" />
+                <div className="group relative overflow-hidden md:row-span-2 ">
+                <img src="https://png.pngtree.com/background/20250315/original/pngtree-simple-space-view-mobile-wallpaper-picture-image_15525419.jpg" alt="Description" className="cursor-pointer h-full w-full object-cover rounded-lg" />
+                <div className="absolute bottom-0 w-full cursor-pointer bg-gradient-to-t from-black via-gray to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex flex-col justify-end p-4">
+                <h3 className="text-white text-xl font-semibold">Nebula View</h3>
+                <p className="text-gray-200 text-sm">Deep space exploration</p>
+                </div>
+                </div>
                 <div className="group relative overflow-hidden ">
                 <img className= "h-full w-full object-cover cursor-pointer rounded-lg overflow-clip" src="https://img.magnific.com/free-photo/ultra-detailed-nebula-abstract-wallpaper-4_1562-749.jpg?semt=ais_hybrid&w=740&q=80" alt="space" />
                 <div className="absolute bottom-0 w-full cursor-pointer bg-gradient-to-t from-black via-gray to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex flex-col justify-end p-4">
