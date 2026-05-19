bb6fadd16ee256a9a23d8cfc35b836e7a8589945
Sahar Atat <saharatat325@gmail.com>
2026-05-19 13:02:48 +0300

    spacing

--- Components/subMenuBanner.js
+++ Components/subMenuBanner.js
@@ -1,7 +1,7 @@
 export default function subMenuBanner() {
   return (
     <section
-      className="relative h-[350px] bg-cover bg-center  <container mx-auto px-6 md:px-20"
+      className="relative h-[350px] bg-cover bg-center  "
       style={{
         backgroundImage:
           "url('https://images.unsplash.com/photo-1507525428034-b723cf961d3e')",
@@ -11,7 +11,7 @@ export default function subMenuBanner() {
       <div className="absolute inset-0 bg-black/20" />
      
 
-      <div className="relative z-10 flex items-center h-full max-w-7xl mx-auto px-6">
+      <div className="relative z-10 flex items-center h-full container mx-auto px-6 md:px-20">
         <h1 className="text-white text-6xl font-bold">
           Company Page
         </h1>
