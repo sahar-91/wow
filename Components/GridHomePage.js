52617610ffe53d1a764911596a3cb93e03bc5300
Sahar Atat <saharatat325@gmail.com>
2026-05-20 13:44:06 +0300

    skeleton in grid

--- Components/GridHomePage.js
+++ Components/GridHomePage.js
@@ -8,16 +8,29 @@ import Link from "next/link";
 import SearchBarCard from "./SearchBarCard";
 
 export default function GridHomePage() {
+
+    const [items, setItems] = useState([])
+    const [query, setQuery] = useState("");
+    const [loading, setLoading] = useState(true);
+
    useEffect(() => {
     const fetchData = async () => {
+      try{
       const res = await fetch("https://hanzo.dxpshift.com/api/projects");
       const data = await res.json();
       setItems(data.data || []);
+      }
+      catch (error) {
+      console.error(error);
+      }
+      finally {
+      setLoading(false);
+      }
+      
     };
     fetchData();
   }, []);
-    const [items, setItems] = useState([])
-    const [query, setQuery] = useState("");
+
     const filteredItems = items.filter((item) => {
     const title = item.title?.toLowerCase() || "";
     const desc = item.description?.toLowerCase() || "";
@@ -30,6 +43,7 @@ const sectionRef = useRef(null);
 const cardsRef   = useRef([]);
 
 useEffect(() => {
+  if (loading) return;
   cardsRef.current.forEach((card) => {
     if (!card) return;
 
@@ -53,7 +67,7 @@ useEffect(() => {
       }
     );
   });
-}, [filteredItems]);
+}, [filteredItems, loading]);
 
 
     return (
@@ -63,7 +77,18 @@ useEffect(() => {
             <div className="container mx-auto max-w-[1440px] px-4 md:px-60">
                  <SearchBarCard query={query} setQuery={setQuery} /> 
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-y-2 gap-x-6 items-start">
-                    {filteredItems.map((item, index) => (
+                  {loading ? (
+                    Array.from({ length: 4 }).map((_, i) => (
+      <div key={i} className="mb-5 animate-pulse">
+        <div className="w-full aspect-video bg-gray-800 rounded-md" />
+        <div className="py-4 space-y-2">
+          <div className="h-6 bg-gray-800 rounded w-3/4" />
+          <div className="h-4 bg-gray-800 rounded w-1/2" />
+        </div>
+      </div>
+                    ))
+                  ) : (
+                    filteredItems.map((item, index) => (
                         <Link key={item.id} href={`/cardDetails/${item.id}`}>
                         <div ref={(el) => (cardsRef.current[index] = el)} className="bg-black cursor-pointer mb-5 h-auto overflow-clip">
                             <div className="w-full h-full overflow-hidden">
@@ -75,6 +100,7 @@ useEffect(() => {
                     </div>
                 </div>
                 </Link>
+                    )            
             ))}
         </div>
         </div>
